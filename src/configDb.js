77d8b26e2bf4b22f68022616b6dbc726ef092345
ismael-henrique-dev <ismael.henrique.dev@gmail.com>
2024-06-01 11:03:55 -0300

    process.env integraded

--- src/configDb.js
+++ src/configDb.js
@@ -1,10 +1,11 @@
 import sqlite3 from "sqlite3"
 import { open } from "sqlite"
+import "dotenv/config"
 
 // you would have to import / invoke this in another file
 export async function openDb() {
   return open({
-    filename: "./database.db",
+    filename: process.env.DATABASE_URL,
     driver: sqlite3.Database,
   })
 }
