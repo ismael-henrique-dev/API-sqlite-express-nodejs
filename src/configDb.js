8d4f7e5861c93a3d40978d12b4256700cd1ad248
ismael-henrique-dev <ismael.henrique.dev@gmail.com>
2024-06-02 12:49:28 -0300

    finished

--- src/configDb.js
+++ src/configDb.js
@@ -5,7 +5,7 @@ import "dotenv/config"
 // you would have to import / invoke this in another file
 export async function openDb() {
   return open({
-    filename: process.env.DATABASE_URL,
+    filename: "./database.db",
     driver: sqlite3.Database,
   })
 }
